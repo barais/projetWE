1befcfccbefed76b3a136b849032f083026c1e20
Olivier Barais <barais@irisa.fr>
2018-11-23 16:04:54 +0100

    last

--- src/main/java/fr/ensai/projetWE/App.java
+++ src/main/java/fr/ensai/projetWE/App.java
@@ -1,5 +1,13 @@
 package fr.ensai.projetWE;
 
+import java.sql.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.transaction.Transactional;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Ensai" );
+
+    	EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("mysql");
+		EntityManager manager = factory.createEntityManager();
+		
+		User u = new User();
+		u.setDatenaissance(new Date(80, 01, 26));
+		u.setNom("barais");
+		u.setPrenom("Olivier");
+		u.setRole(ROLE.ADMIN);
+
+		
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		try {
+			manager.persist(u);
+
+		
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		tx.commit();
+		
+		manager.close();
+		factory.close();
+
     }
+    
 }
--- src/main/java/fr/ensai/projetWE/User.java
+++ src/main/java/fr/ensai/projetWE/User.java
@@ -1,5 +1,67 @@
 package fr.ensai.projetWE;
 
-public class User {
+import java.sql.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	
+	@Id@GeneratedValue
+	long id;
+	
+	String nom, prenom;
+	Date datenaissance;
+	ROLE role;
+	List<Activite> sportspreferes;
+	List<ActiviteHistorique> historiques;
+	
+	public List<ActiviteHistorique> getHistoriques() {
+		return historiques;
+	}
+	public void setHistoriques(List<ActiviteHistorique> historiques) {
+		this.historiques = historiques;
+	}
+	public List<Activite> getSportspreferes() {
+		return sportspreferes;
+	}
+	public void setSportspreferes(List<Activite> sportspreferes) {
+		this.sportspreferes = sportspreferes;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDatenaissance() {
+		return datenaissance;
+	}
+	public void setDatenaissance(Date datenaissance) {
+		this.datenaissance = datenaissance;
+	}
+	public ROLE getRole() {
+		return role;
+	}
+	public void setRole(ROLE role) {
+		this.role = role;
+	}
+	
+	
 }
